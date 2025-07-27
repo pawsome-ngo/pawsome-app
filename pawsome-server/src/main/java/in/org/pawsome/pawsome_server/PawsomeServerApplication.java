6d9b0edb515c2cba5ad8408aa193576dc58e0f0f
repo-docs-local <debayanthenroy@gmail.com>
2025-07-27 19:03:07 +0530

    First Commit

---
+++ pawsome-server/src/main/java/in/org/pawsome/pawsome_server/PawsomeServerApplication.java
@@ -0,0 +1,13 @@
+package in.org.pawsome.pawsome_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsomeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsomeServerApplication.class, args);
+	}
+
+}
